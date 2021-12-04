15bf26d02c44d54063c291c8a94d18fdeb670de6
Manasvi Kale <66360873+manasvi-16@users.noreply.github.com>
2021-12-04 21:36:10 +0530

    Add files via upload
    This is a python script that has a function that takes two string inputs from the user, concatenates them, and returns them in - "city, country" format.
    For example, you pass "Mumbai" as a city and "India" as the country, the output will look like = Mumbai, India.

---
+++ city_functions.py
@@ -0,0 +1,6 @@
+#This pgm takes two strings concatenates them and returns to the user
+
+def city_function(city,country):
+    """Concatenates values inserted by user."""
+    conc = f"{city}, {country}"
+    return conc.title()
